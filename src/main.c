45b08a4cca7362db1bd5f82b0ec70d1bf73ac882
ChillerDragon <ChillerDragon@gmail.com>
2024-05-01 17:04:47 +0800

    what am i doing with my life

--- src/main.c
+++ src/main.c
@@ -5,10 +5,24 @@ struct SGlobalState
     Font font;
 };
 
+struct SRect
+{
+    int x;
+    int y;
+    int w;
+    int h;
+    Color color;
+};
+
 
 void Text(struct SGlobalState *g, const char *text, Vector2 position)
 {
         DrawTextEx(g->font, "foo", (Vector2){ 20.0f, 100.0f }, (float)g->font.baseSize * 4, 2, LIME);
+}
+
+void DrawRect(struct SRect *rect)
+{
+        DrawRectangle(rect->x, rect->y, rect->w, rect->h, rect->color);
 }
 
 int main(void)
@@ -22,6 +36,10 @@ int main(void)
     // g.font = LoadFont("data/font.png");
     g.font = LoadFontEx("data/knowyour.ttf", 32, 0, 250);
 
+    struct SRect rect = {
+        0, 0, 10, 10, RED
+    };
+
     while (!WindowShouldClose())
     {
         // Update
@@ -29,12 +47,17 @@ int main(void)
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
             TraceLog(LOG_INFO, "mouse %f %f", mouse.x, mouse.y);
+            rect.x = mouse.x;
+            rect.y = mouse.y;
         }
 
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
         Text(&g, "foo", (Vector2){ 20.0f, 100.0f });
+
+        DrawRect(&rect);
+
         EndDrawing();
     }
 
